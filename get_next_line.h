d3544edc85fd0d76ef1bbf40b8eff801e2fbba73
seriiin <selin@choeselin-ui-MacBookPro.local>
2021-02-16 23:33:50 +0900

    헤더파일

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: selin <selin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 17:53:08 by selin             #+#    #+#             */
-/*   Updated: 2021/02/10 17:53:08 by selin            ###   ########.fr       */
+/*   Updated: 2021/02/16 23:33:39 by selchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
