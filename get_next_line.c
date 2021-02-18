a4ccd8e1199306547a5ccb6f8dbbc2a85db898e8
seriiin <csl602@likelion.org>
2021-02-18 15:10:55 +0900

    몰라

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: selin <selin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 17:52:59 by selin             #+#    #+#             */
-/*   Updated: 2021/02/16 23:52:22 by selchoi          ###   ########.fr       */
+/*   Updated: 2021/02/18 15:10:13 by selchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ int get_next_line(int fd, char **line)
 {
 	char 		buf[BUFFER_SIZE];
 	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line)
+		return (-1);
+
+	read(fd, buf, BUFFER_SIZE);
+	
 
 }
